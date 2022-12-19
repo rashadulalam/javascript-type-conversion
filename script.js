f868f470df5b4ff47cb002b84333c16167600667
rashadulalam <rashadul91@gmail.com>
2022-12-19 20:18:29 +0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. Type conversion
+
+// const innputNumber = "7500";
+// console.log(innputNumber + 1);
+
+// console.log( Number (innputNumber) + 10);
+
+// console.log(innputNumber)
+// console.log(Number(innputNumber), innputNumber);
+// console.log(typeof innputNumber)
+
+const birthYear = 1988;
+console.log(String(birthYear));
+console.log(String(birthYear));
+
+const persion = "Adrita";
+
+// check type
+console.log(typeof (persion))
+
+// convert to number
+console.log(Number (persion))

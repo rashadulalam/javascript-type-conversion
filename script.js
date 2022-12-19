a5753e5f963a6179388bd7a81014655820c90c04
rashadulalam <rashadul91@gmail.com>
2022-12-19 20:22:12 +0600

    Convert to Boolean

--- script.js
+++ script.js
@@ -9,14 +9,21 @@
 // console.log(Number(innputNumber), innputNumber);
 // console.log(typeof innputNumber)
 
-const birthYear = 1988;
-console.log(String(birthYear));
-console.log(String(birthYear));
+// const birthYear = 1988;
+// console.log(String(birthYear));
+// console.log(String(birthYear));
 
-const persion = "Adrita";
+// const persion = "Adrita";
 
 // check type
-console.log(typeof (persion))
+// console.log(typeof (persion))
 
-// convert to number
-console.log(Number (persion))
+// // convert to number
+// console.log(Number (persion))
+
+
+// convert to Boolean
+const innputNumber = "7500";
+const firstName = "Adrita";
+console.log(Boolean(innputNumber));
+console.log(Boolean(firstName));

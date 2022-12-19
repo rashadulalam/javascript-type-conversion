cdf8416c4d83a07f20886956107e5fdb7b9673cd
rashadulalam <rashadul91@gmail.com>
2022-12-19 20:45:59 +0600

    JavaScript Truthy and falsy value

--- script.js
+++ script.js
@@ -32,11 +32,39 @@
 
 // 2. JavaScript Type Coercion
 // JavaScript Auto data conversion process
-let bookNumber = 5;
-console.log("I have " + bookNumber + " Story Book");
-console.log("20" - 5)
-console.log("20" / 5);
+// let bookNumber = 5;
+// console.log("I have " + bookNumber + " Story Book");
+// console.log("20" - 5)
+// console.log("20" / 5);
 
-console.log( 5 + 10 + 5 + "5");
-console.log("100" - "50" + 10);
-console.log("15" - "5" - "4" + "5")
+// console.log( 5 + 10 + 5 + "5");
+// console.log("100" - "50" + 10);
+// console.log("15" - "5" - "4" + "5")
+
+
+// 3. JavaScript Truthy and falsy value
+// console.log(0);
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(1));
+// console.log(Boolean("Adrita"));
+// console.log(Boolean("123"));
+
+const job = 1;
+
+if( job ) {
+    console.log("I have a Good Job!");
+} else {
+    console.log("Looking for a Good Job!");
+}
+
+let salary;
+
+if( salary ) {
+    console.log("My salary is set!" );
+} else {
+    console.log("My salary isn't set!" );
+}
